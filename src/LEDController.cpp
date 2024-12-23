36d8aa542693b7e1141448a7fa17c8f02e3e7c64
Sherman <tw.sherman.chang@gmail.com>
2024-12-23 17:08:53 +0800

    small changes

--- src/LEDController.cpp
+++ src/LEDController.cpp
@@ -9,35 +9,35 @@ const std::string GPIO_BASE_PATH = "/sys/class/gpio/";
 // LEDColor
 LEDColor::LEDColor() : r(0), g(0), b(0), rgb(0) {}
 void LEDColor::setColor(const int &colorCode) {
-  const int R = (colorCode >> 24) & 0xff;
-  const int G = (colorCode >> 16) & 0xff;
-  const int B = (colorCode >> 8) & 0xff;
-  const int A = (colorCode >> 0) & 0xff;
+	const int R = (colorCode >> 24) & 0xff;
+	const int G = (colorCode >> 16) & 0xff;
+	const int B = (colorCode >> 8) & 0xff;
+	const int A = (colorCode >> 0) & 0xff;
 
-  float r_cal = 0.0, g_cal = 0.0, b_cal = 0.0;
-  if ((R + G + B) > 0) {
-    float a = A / 255.0;
-    r_cal = (1.0) * (R / 255.0) * a;
-    g_cal = (1.0) * (G / 255.0) * a;
-    b_cal = (1.0) * (B / 255.0) * a;
+	float r_cal = 0.0, g_cal = 0.0, b_cal = 0.0;
+	if ((R + G + B) > 0) {
+		float a = A / 255.0;
+		r_cal = (1.0) * (R / 255.0) * a;
+		g_cal = (1.0) * (G / 255.0) * a;
+		b_cal = (1.0) * (B / 255.0) * a;
 
-    r_cal = pow(r_cal, Config::GAMMA_LED_R);
-    g_cal = pow(g_cal, Config::GAMMA_LED_G);
-    b_cal = pow(b_cal, Config::GAMMA_LED_B);
+		r_cal = pow(r_cal, Config::GAMMA_LED_R);
+		g_cal = pow(g_cal, Config::GAMMA_LED_G);
+		b_cal = pow(b_cal, Config::GAMMA_LED_B);
 
-    r_cal *= Config::LED_MAX_BRIGHTNESS;
-    g_cal *= Config::LED_MAX_BRIGHTNESS;
-    b_cal *= Config::LED_MAX_BRIGHTNESS;
+		r_cal *= Config::LED_MAX_BRIGHTNESS;
+		g_cal *= Config::LED_MAX_BRIGHTNESS;
+		b_cal *= Config::LED_MAX_BRIGHTNESS;
 
-    r = int(r_cal);
-    g = int(g_cal);
-    b = int(b_cal);
+		r = int(r_cal);
+		g = int(g_cal);
+		b = int(b_cal);
 
-    rgb = (r << 16) + (g << 8) + b;
-  } else {
-    r = g = b = 0;
-    rgb = 0;
-  }
+		rgb = (r << 16) + (g << 8) + b;
+	} else {
+		r = g = b = 0;
+		rgb = 0;
+  	}
 }
 uint32_t LEDColor::getRGB() { return rgb; }
 
@@ -101,22 +101,20 @@ int LEDController::sendAll(const std::vector<std::vector<int>> &statusLists) {
   return 0;
 }
 int LEDController::play(const std::vector<std::vector<int>> &statusLists) {
-  ws2811_return_t ret;
-  LEDColor led;
-  for (int i = 0; i < num_channel; i++) {
-    select_channel(i);
-    for (int j = 0; j < ledString[i].channel[0].count; j++) {
-      led.setColor(statusLists[i][j]);
-      ledString[i].channel[0].leds[j] = led.getRGB();
-    }
-
-    if ((ret = ws2811_render(&ledString[i])) != WS2811_SUCCESS) {
-      //            fprintf(stderr, "ws2811_render %d failed: %s\n", i,
-      //            ws2811_get_return_t_str(ret));
-      return ret;
-    }
-    usleep(ledString[i].channel[0].count * 30);
-  }
+  	ws2811_return_t return_WS2811;
+  	LEDColor led;
+  	for (int i = 0; i < num_channel; i++) {
+    	select_channel(i);
+		for (int j = 0; j < ledString[i].channel[0].count; j++) {
+			led.setColor(statusLists[i][j]);
+			ledString[i].channel[0].leds[j] = led.getRGB();
+		}
+		if ((return_WS2811 = ws2811_render(&ledString[i])) != WS2811_SUCCESS) {
+			// fprintf(stderr, "ws2811_render %d failed: %s\n", i, ws2811_get_return_t_str(return_WS2811));
+			return return_WS2811;
+		}
+		usleep(ledString[i].channel[0].count * 30);
+  	}
   return 0;
 }
 void LEDController::gpioInit() {
@@ -221,31 +219,30 @@ void LEDController::select_channel(int address) {
 }
 
 void LEDController::setValue(int pin, int value) {
-  std::ofstream valueFile(GPIO_BASE_PATH + "gpio" + std::to_string(pin) +
-                          "/value");
-  if (!valueFile.is_open()) {
-    throw std::runtime_error("無法設置 GPIO " + std::to_string(pin) + " 的值");
-  }
-  valueFile << value;
-  valueFile.close();
+  	std::ofstream valueFile(GPIO_BASE_PATH + "gpio" + std::to_string(pin) + "/value");
+  	if (!valueFile.is_open()) {
+    	throw std::runtime_error("Can't set GPIO pin" + std::to_string(pin) + "'s value");
+  	}
+	valueFile << value;
+	valueFile.close();
 }
 
 void LEDController::finish() {
   for (int i = 0; i < num_channel; i++)
     ws2811_fini(&ledString[i]);
-  printf("LED GPIO finished.\n");
-  close(A0);
-  close(A1);
-  close(A2);
+  	printf("LED GPIO finished.\n");
+  	close(A0);
+  	close(A1);
+  	close(A2);
 }
 void LEDController::close_gpio() {
-  int fd = open("/sys/class/gpio/unexport", O_WRONLY);
-  if (fd == -1) {
-    perror("Unable to open /sys/class/gpio/unexport");
-    exit(1);
-  }
+	int fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	if (fd == -1) {
+		perror("Unable to open /sys/class/gpio/unexport");
+		exit(1);
+  	}
 
-  if (write(fd, "23", 2) != 2) {
+  	if (write(fd, "23", 2) != 2) {
     fprintf(stderr, "Error writing to /sys/class/gpio/unexport: 23\n");
   }
 
